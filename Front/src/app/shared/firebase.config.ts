a544041fb4af3825ee9810acd97c71e432a118bc
NicolasGarayC <jngaray42@ucatolica.edu.co>
2024-11-06 21:46:01 -0500

    se añaden variables de entorno

--- Front/src/app/shared/firebase.config.ts
+++ Front/src/app/shared/firebase.config.ts
@@ -4,7 +4,15 @@ import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 
 export const firebaseConfig = {
-  };
+  apiKey: "AIzaSyANqnnqdq546BgLQXgzoICyitI3G69qwm0",
+  authDomain: "webproj-6ccd8.firebaseapp.com",
+  projectId: "webproj-6ccd8",
+  storageBucket: "webproj-6ccd8.appspot.com",
+  messagingSenderId: "664211464690",
+  appId: "1:664211464690:web:95dbe9c8f64cb093a34b6b",
+  measurementId: "G-J1GLXTCWS3"
+
+};
 
 export const app = initializeApp(firebaseConfig);
 export const analytics = getAnalytics(app);
